135350fbec286f4ce6d523a459ebdfaedfa463bb
default <marco.cantusci@stu.ocsb.ca>
2025-09-18 16:49:00 +0000

    I added done statement, doc comment, and inline comment

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
+/*
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-18
+ * @fileoverview This is the traditional "Hello, World!" program.
+ */
+
 package main
 
 import "fmt"
 
 func main() {
+	// print out the greeting
 	fmt.Println("Hello, World!")
+	fmt.Println("Done.")
 }
