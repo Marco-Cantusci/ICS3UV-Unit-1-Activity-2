98d66342094fbe32dcf416413024f3d8521f9839
default <marco.cantusci@stu.ocsb.ca>
2025-09-18 16:13:56 +0000

    I have added the  doc comment, and the done statement.

---
+++ main.ts
@@ -0,0 +1,12 @@
+/**
+ * @author Marco Cantusci
+ * @verion 1.0.0
+ * @date 2025-09-18
+ * @fileoverview This is the traditional "Hello, World!" program.
+ */
+
+// print out the greeting
+console.log("Hello, World!");
+
+console.log("\ndone.");
+
